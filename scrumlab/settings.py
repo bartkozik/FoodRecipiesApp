4aedc42ffedd08afb07bdca144d5d046dd32e706
Bartosz Kozikowski <106819506+bartkozik@users.noreply.github.com>
2022-10-12 16:43:19 +0200

    Update settings.py

--- scrumlab/settings.py
+++ scrumlab/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rtm$@rcneyyjbo$u+)@z1dxy!qrtxc676h&zh97s+46)=2)@9*'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
